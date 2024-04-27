e37df07a996b7d13c0b35e1bbdbc6cfd8beecf19
PikakueNNZ <40914469+PikakueNNZ@users.noreply.github.com>
2024-04-27 07:02:16 +0000

    change zmk_mouse_hog_send_mouse_report

--- app/src/mouse/hog.c
+++ app/src/mouse/hog.c
@@ -167,7 +167,7 @@ void send_mouse_report_callback(struct k_work *work) {
 K_WORK_DEFINE(hog_mouse_work, send_mouse_report_callback);
 
 int zmk_mouse_hog_send_mouse_report(struct zmk_hid_mouse_report_body *report) {
-    int err = k_msgq_put(&zmk_hog_mouse_msgq, report, K_MSEC(100));
+    int err = k_msgq_put(&zmk_hog_mouse_msgq, report, K_MSEC(1));
     if (err) {
         switch (err) {
         case -EAGAIN: {
